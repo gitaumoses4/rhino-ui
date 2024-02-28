22b0600c3d4fc3e44e8de43240f2e128f4eb7ac9
Moses Gitau <gitaumoses4@gmail.com>
2024-02-28 23:03:43 +0300

    use configuration packages for eslint, stylelint and tsconfig (#64)

--- libs/eslint-config/src/index.js
+++ libs/eslint-config/src/index.js
@@ -1,125 +1,105 @@
 module.exports = {
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "extends": [
+  root: true,
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: [".turbo", "dist", "coverage", "node_modules", "build"],
+  extends: [
     "airbnb",
     "airbnb/hooks",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:json/recommended",
   ],
-  "plugins": [
-    "jsx-a11y",
-    "@typescript-eslint"
-  ],
-  "env": {
-    "browser": true
+  plugins: ["jsx-a11y", "@typescript-eslint"],
+  env: {
+    browser: true,
   },
-  "rules": {
+  rules: {
     "react/require-default-props": "off",
     "consistent-return": "off",
     "import/prefer-default-export": "off",
+    "object-curly-spacing": ["error", "always"],
     "no-prototype-builtins": "off",
-    "arrow-parens": [
-      "error",
-      "as-needed"
-    ],
-    "import/no-extraneous-dependencies": [
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "prettier/prettier": [
       "error",
       {
-        "devDependencies": true
-      }
+        tabWidth: 2,
+        printWidth: 120,
+        semi: true,
+        useTabs: false,
+        singleQuote: false,
+        endOfLine: "auto",
+        arrowParens: "always",
+        trailingComma: "all",
+      },
     ],
     "react/jsx-filename-extension": [
       1,
       {
-        "extensions": [
-          ".jsx",
-          "tsx"
-        ]
-      }
+        extensions: [".jsx", "tsx"],
+      },
     ],
-    "react/jsx-props-no-spreading": [
-      0
-    ],
-    "max-len": [
-      1,
-      120
-    ],
+    "react/jsx-props-no-spreading": [0],
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error"
-    ],
+    "@typescript-eslint/no-unused-vars": ["error"],
     "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": [
-      "error"
-    ],
+    "no-await-in-loop": "off",
+    "no-plusplus": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
     "import/extensions": [
       "error",
       "ignorePackages",
       {
-        "js": "never",
-        "mjs": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
+        js: "never",
+        mjs: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
     ],
     "@typescript-eslint/member-delimiter-style": [
       "error",
       {
-        "multiline": {
-          "delimiter": "semi",
-          "requireLast": true
+        multiline: {
+          delimiter: "semi",
+          requireLast: true,
         },
-        "singleline": {
-          "delimiter": "semi",
-          "requireLast": true
-        }
-      }
+        singleline: {
+          delimiter: "semi",
+          requireLast: false,
+        },
+      },
     ],
-    "object-curly-newline": "off"
+    "object-curly-newline": "off",
   },
-  "settings": {
+  settings: {
     "import/parsers": {
-      "@typescript-eslint/parser": [
-        ".ts",
-        ".tsx"
-      ]
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
     "import/resolver": {
-      "typescript": {
-        "alwaysTryTypes": true
-        // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      typescript: {
+        alwaysTryTypes: true,
       },
-      "node": {
-        "extensions": [
-          ".js",
-          ".jsx",
-          ".ts",
-          ".tsx"
-        ]
-      }
-    }
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
   },
-  "overrides": [
+  overrides: [
     {
-      "files": [
-        "**/*.js"
-      ],
-      "rules": {
+      files: ["**/*.js"],
+      rules: {
         "@typescript-eslint/no-var-requires": 0,
-        "@typescript-eslint/explicit-module-boundary-types": 0
-      }
+        "@typescript-eslint/explicit-module-boundary-types": 0,
+      },
     },
     {
-      "files": [
-        "**/*.tsx"
-      ],
-      "rules": {
-        "react/prop-types": "off"
-      }
-    }
-  ]
+      files: ["**/*.tsx"],
+      rules: {
+        "react/prop-types": "off",
+      },
+    },
+  ],
 };
